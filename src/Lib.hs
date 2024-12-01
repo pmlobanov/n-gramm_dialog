aa39d4538b067e3bcb1b32f1b36759fda12a3121
pmlobanov <lobanovpm@yandex.ru>
2024-12-01 22:20:17 +0300

    added sentence parser from string

---
+++ src/Lib.hs
@@ -0,0 +1,43 @@
+module Lib
+    ( parseText) where
+
+import Control.Applicative
+import Data.Char
+newtype Parser tok a = Parser { runParser :: [tok] -> Maybe ([tok],a) }
+
+instance Functor (Parser tok) where
+    fmap g = Parser . (fmap . fmap . fmap) g . runParser
+
+instance Applicative (Parser tok) where 
+    pure x =Parser $ \s-> Just(s,x)
+    Parser u <*> Parser v = Parser f where
+        f xs = case u xs of 
+            Nothing       -> Nothing
+            Just (xs', g) -> case v xs' of 
+                Nothing        -> Nothing
+                Just (xs'', x) -> Just (xs'', g x)
+
+instance Alternative (Parser tok) where
+    empty = Parser $ const Nothing
+    
+    Parser u <|> Parser v = Parser f where 
+        f xs = case u xs of
+         Nothing -> v xs
+         z       -> z
+
+satisfy :: (tok -> Bool) -> Parser tok tok
+satisfy pr = Parser f where
+    f (c:cs) | pr c = Just (cs,c)
+    f _ = Nothing
+
+wordParser :: Parser Char Char
+wordParser = satisfy (\c -> not (isPunctuation c) && not (isDigit c))
+
+sentenceDelimiter :: Parser Char Char
+sentenceDelimiter = satisfy (`elem` ".!?:;()")
+
+sentenceParser :: Parser Char String
+sentenceParser = some wordParser <* optional (some (satisfy isDigit)) <* optional ( satisfy isSeparator)
+
+parseText :: Parser Char [String]
+parseText = some (sentenceParser <* sentenceDelimiter <* optional ( satisfy isSeparator)) 
