a6c506972aace1c78706210888cbaebb5b5bb4b0
pmlobanov <lobanovpm@yandex.ru>
2024-12-05 00:47:46 +0300

    feat: dictionary function added

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,7 +3,13 @@ module Lib
 
 import Control.Applicative
 import Data.Char
+import Data.List 
+import Data.Maybe
+import Data.Map (Map)
+import qualified Data.Map as Map
+
 newtype Parser tok a = Parser { runParser :: [tok] -> Maybe ([tok],a) }
+type Pair = (String, String)
 
 instance Functor (Parser tok) where
     fmap g = Parser . (fmap . fmap . fmap) g . runParser
@@ -31,11 +37,10 @@ satisfy pr = Parser f where
     f _ = Nothing
 
 isleftSymbol :: Parser Char Char
-isleftSymbol = satisfy (\c -> (isPunctuation c  || isSymbol c ) && not (c `elem` ".!?:;()" ))
-
+isleftSymbol = satisfy (\c -> (isPunctuation c  || isSymbol c) && not (c `elem` ".!?:;() " ))
 
 wordParser :: Parser Char Char
-wordParser = optional isleftSymbol *> satisfy (\c -> not (isPunctuation c) && not (isDigit c)) <* optional isleftSymbol
+wordParser = optional isleftSymbol *> satisfy (\c -> not (isPunctuation c) && not (isDigit c) {-&& not (isSeparator c)-}) <* optional isleftSymbol
 
 sentenceDelimiter :: Parser Char Char
 sentenceDelimiter = satisfy (`elem` ".!?:;()")
@@ -48,7 +53,71 @@ parseText = some (sentenceParser <* sentenceDelimiter <* optional ( satisfy isSe
 
 getparsedText :: String -> [String]
 getparsedText input = case runParser parseText input of
-    Just (rest, sentences) -> sentences
+    Just (_, sentences) -> sentences
     Nothing -> []
 
 -- test text "a b, c d e! b c d? e b c# a ^d. a f; f."
+
+--todo Написать функцию getnext getnext2, которая берет следующее слово после пробела.
+
+wordParserClean :: Parser Char String
+wordParserClean=  some (satisfy isAlpha)
+
+sentenceParserClean :: Parser Char [String]
+sentenceParserClean = some (wordParserClean <* optional (satisfy isSeparator))
+
+textConvert :: [String] -> [[String]]
+textConvert [] = []
+textConvert (s:ss) = case runParser sentenceParserClean s of
+    Just (_, sentences) -> sentences : textConvert ss
+    Nothing -> []
+
+getUniqueWords :: String -> [String]
+getUniqueWords text= nub (mconcat (textConvert (getparsedText text))) 
+
+bigrams :: [String] -> [(String, String)]
+bigrams [] = []
+bigrams [x] = [(x," ")]
+bigrams (x:xs) = (x, head xs) : bigrams  xs
+
+trigrams :: [String] -> [(String, String)]
+trigrams xs  = [(x, y ++ " " ++ z) | (x:y:z:_) <- tails xs]
+
+
+qtrigrams :: [String] -> [(String, String)]
+qtrigrams xs  = [(x ++ " " ++ y, z) | (x:y:z:_) <- tails xs]
+
+
+trigramsFromLists :: [[String]] -> [(String, String)]
+trigramsFromLists xs=  concatMap trigrams (filter (\ys -> length ys >= 3) xs) ++ concatMap qtrigrams (filter (\ys -> length ys >= 3) xs) ++ concatMap bigrams (filter (\ys -> length ys >= 2) xs)
+--createdictionary :: [(String,String)]-> Map.Map String String
+--createdictionary (s:ss) =   
+{-
+-- Функция для добавления значения в список по ключу в словаре
+addToMap :: (Ord k) => k -> v -> Map.Map k [v] -> Map.Map k [v]
+addToMap key value = Map.insertWith (++) key [value]
+addToMap _ "" = if Map.lookup 
+-}
+
+
+addPair :: Pair -> Map String [String] -> Map String [String]
+addPair (k, v) m
+  | all (== ' ') v = Map.insert k [] m
+  | otherwise =  Map.insertWith (\new old -> nub (new ++ old)) k [v] m
+
+buildMap :: [Pair] -> Map String [String]
+buildMap = foldr addPair Map.empty
+
+
+{-
+addToMap :: (Ord k) => k-> String -> Map.Map k [String] -> Map.Map k [String]
+addToMap key value m
+  | value == "1" =  case Map.lookup key m of
+                      Nothing -> Map.insert key [value] m
+                      Just existingValue -> Map.insertWith (++) key (existingValue ++ ["2"]) m --if existingValue == [""] then Map.insert key [value] m else m
+  | otherwise =  Map.insertWith (++) key [value] m
+
+-- Функция для генерации словаря из списка кортежей
+generateDictionary :: (Ord k) => [(k, String )] -> Map.Map k [String ]
+generateDictionary = foldr (uncurry addToMap) Map.empty
+-}
