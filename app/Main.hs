c8e7dac6a2a7880faca6627455aaa9b3d18e519a
pmlobanov <lobanovpm@yandex.ru>
2024-12-07 22:00:29 +0300

    i

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,55 @@
-module Main (main) where
+module Main where
 
-import Lib
+import Lib 
 
 main :: IO ()
-main = putStrLn("1")
+main =
+    putStrLn "\nChoose an option:" >>
+    putStrLn "1. Text file preprocessing" >>
+    putStrLn "2. Interact with model" >>
+    putStrLn "4. Models dialogue" >>
+    putStrLn "5. Exit" >>
+    getLine >>= \choice ->
+    case choice of
+      "1" ->
+        putStrLn "Enter input file name:" >> 
+        getLine >>= \ inputFileName ->
+        putStrLn "Enter output file name:" >> getLine >>=
+        \ outputFileName-> part1 inputFileName outputFileName >> main
+      "2" ->  putStrLn "Enter dictionary file name:" >>
+        getLine >>= part2 >>
+        main
+      "5" -> putStrLn "Good Buy!"
+
+part1:: String -> String  -> IO ()
+part1 inputFileName outputFileName = do 
+    --readFile inputFileName >>= \input -> getparsedText input >> putStrLn "\nCompleted successfully!\n"
+    input <- readFile inputFileName
+   -- putStrLn input
+    let res = getparsedText input
+    --putStrLn (show res)
+    putStrLn "\nRecognized sentences:\n"
+    printRes res
+    let dictionary = buildMap(trigramsFromLists (textConvert res))
+    writeFile outputFileName (dictToString $ dictionary)
+    putStrLn "\n Dictionary based on recognized sentences are written to the file\n"
+    {-input2 <- readFile outputFileName
+    putStrLn  input2
+    putStrLn "\nRecognized sentences:\n"
+    
+    putStrLn "\nВведите слово или пару слов:">>
+     getLine >>= \input -> 
+     generateRandomString dictionary input >>= \result ->
+     putStrLn result
+    -}
+part2 :: String -> IO ()
+part2 dictFilename = 
+  parseFileToDict dictFilename >>= \dictionary ->
+  generateSentences dictionary 
+  --generateSentences dictionary  
+ -- putStrLn "\n Dictionary based on recognized sentences are written to the file\n"
+
+
+printRes:: [String] -> IO ()
+printRes = foldr ((>>) . putStrLn) (return ())
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,12 @@
-module Lib
-    ( parseText) where
+module Lib (getparsedText, textConvert, trigramsFromLists, generateRandomString, buildMap, dictToString, generateSentences, parseFileToDict) where
 
 import Control.Applicative
 import Data.Char
 import Data.List 
 import System.Random (randomRIO)
 import Data.Map (Map)
+import Data.Map (Map, fromListWith, toList, keys, insert, fromList, (!?))
+import Text.Printf (printf)
 import qualified Data.Map as Map
 
 newtype Parser tok a = Parser { runParser :: [tok] -> Maybe ([tok],a) }
@@ -40,7 +41,7 @@ isleftSymbol :: Parser Char Char
 isleftSymbol = satisfy (\c -> (isPunctuation c  || isSymbol c) && not (c `elem` ".!?:;() " ))
 
 wordParser :: Parser Char Char
-wordParser = optional isleftSymbol *> satisfy (\c -> not (isPunctuation c) && not (isDigit c) {-&& not (isSeparator c)-}) <* optional isleftSymbol
+wordParser = optional isleftSymbol *> satisfy (\c -> not (isPunctuation c) && not (isDigit c) {-&& not (isSeparator c)-}) <* optional (some (satisfy isDigit))  <* optional isleftSymbol
 
 sentenceDelimiter :: Parser Char Char
 sentenceDelimiter = satisfy (`elem` ".!?:;()")
@@ -49,7 +50,7 @@ sentenceParser :: Parser Char String
 sentenceParser = some wordParser <* optional (some (satisfy isDigit)) <* optional ( satisfy isSeparator) <* optional isleftSymbol 
 
 parseText :: Parser Char [String]
-parseText = some (sentenceParser <* sentenceDelimiter <* optional ( satisfy isSeparator)) 
+parseText = some (sentenceParser <* sentenceDelimiter <* optional (satisfy ( == '\n')) <* optional ( satisfy isSeparator)) 
 
 getparsedText :: String -> [String]
 getparsedText input = case runParser parseText input of
@@ -105,6 +106,13 @@ buildMap = foldr addPair Map.empty
 randomElement :: [a] -> IO a
 randomElement xs = randomRIO (0, length xs - 1) >>= \idx -> return (xs !! idx)
 
+-- Функция для преобразования словаря в строку для записи в файл
+dictToString :: Map String [String] -> String
+dictToString dict =
+    let dictList = toList dict
+        formatEntry (key, values) = printf "\"%s\" : %s" key (show values)
+    in unlines (map formatEntry dictList)
+
 -- Функция для генерации случайной строки
 generateRandomString :: Map String [String] -> String -> IO String
 generateRandomString m key = generateRandomString' m key 15 [key]
@@ -113,18 +121,51 @@ generateRandomString' :: Map String [String] -> String -> Int -> [String] -> IO 
 generateRandomString' _ _ 0 acc = return $ unwords (reverse acc)
 generateRandomString' m key n acc = 
   case m Map.!? key of
-    Nothing -> if n == 15 then return $ "Ключ " ++ key ++ " не найден в словаре."
-                else return $  "Результат генерации: " ++ unwords (reverse acc) -- ++ show m-- (mconcat acc)
+    Nothing -> if n == 15 then return $ "Key " ++ key ++ "is not contained in the dictionary"
+                else return $  "Generation result: " ++ unwords (reverse acc) -- ++ show m-- (mconcat acc)
     
     Just [] -> return $ unwords (reverse acc) -- ++ key
     Just values -> randomElement values  >>= \next -> generateRandomString' m next (n - 1) (next : acc)
 
-
-main :: IO ()
-main = 
-  let pairs = [("a","b c"),("b","c d"),("c","d e"),("b","c d"),("e","b c"),("b","c a"),("c","a d"),("a b","c"),("b c","d"),("c d","e"),("b c","d"),("e b","c"),("b c","a"),("c a","d"),("a","b"),("b","c"),("c","d"),("d","e"),("e"," "),("b","c"),("c","d"),("d"," "),("e","b"),("b","c"),("c","a"),("a","d"),("d"," "),("a","f"),("f"," ")]
-      dictionary = buildMap pairs
-  in putStrLn "Введите слово или пару слов:">>
+generateSentences :: Map String [String] -> IO ()
+generateSentences dictionary = 
+     putStrLn "Enter word or couple of words:">>
      getLine >>= \input ->
      generateRandomString dictionary input >>= \result ->
      putStrLn result
+
+char :: Char -> Parser Char Char
+char c = satisfy (== c)
+
+string :: String -> Parser Char String
+string = traverse char
+
+spaces :: Parser Char String
+spaces = many (satisfy isSpace)
+
+quotedString :: Parser Char String
+quotedString = char '"' *> many (satisfy (/= '"')) <* char '"'
+
+listParser :: Parser Char [String]
+listParser = char '[' *> spaces *> sepBy quotedString (spaces *> char ',' <* spaces) <* spaces <* char ']'
+
+sepBy :: Parser tok a -> Parser tok sep -> Parser tok [a]
+sepBy p sep = (:) <$> p <*> many (sep *> p) <|> pure []
+
+pairParser :: Parser Char (String, [String])
+pairParser = 
+    (,) <$> (quotedString <* spaces <* char ':' <* spaces) <*> listParser
+
+parseLine :: String -> Maybe (String, [String])
+parseLine input = case runParser pairParser input of
+    Just ("", pair) -> Just pair
+    _ -> Nothing
+
+parseFileToDict :: FilePath -> IO (Map String [String])
+parseFileToDict path = do
+    contents <- readFile path
+    let linesOfFile = lines contents
+        pairs = mapM parseLine linesOfFile
+    case pairs of
+        Just ps -> return (Map.fromList ps)
+        Nothing -> return Map.empty
